46b08365902b9277a6804808e0b575903baa3fa9
James Hobin <hobinjk@mit.edu>
2015-07-29 00:50:09 -0700

    Add basic MIDI playing via MIDI.js

--- piano.js
+++ piano.js
@@ -14,3 +14,20 @@ var parser = new Parser(new InputStream(song));
 parser.parse();
 var notes = parser.notes;
 console.log(notes);
+
+
+window.onload = function () {
+  MIDI.loadPlugin({
+    soundfontUrl: "./lib/soundfont/",
+    instrument: "acoustic_grand_piano",
+    onsuccess: function() {
+      var delay = 0; // play one note every quarter second
+      var note = 50; // the MIDI note
+      var velocity = 127; // how hard the note hits
+      // play the note
+      MIDI.setVolume(0, 127);
+      MIDI.noteOn(0, note, velocity, delay);
+      MIDI.noteOff(0, note, delay + 0.75);
+    }
+  });
+};
